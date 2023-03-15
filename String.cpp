a56342cffec90b74def8956ce5a14c678f235740
Forehm <egortsepenev@icloud.com>
2023-03-15 23:27:57 +0300

    fixed code

--- String.cpp
+++ String.cpp
@@ -8,17 +8,17 @@ public:
 	{
 	public:
 
-		iterator(T* ptr) : ptr_(ptr) { }
+		iterator(char* ptr) : ptr_(ptr) { }
 		iterator operator++() { iterator i = *this; ptr_++; return i; }
 		iterator operator++(int junk) { ptr_++; return *this; }
-		T& operator*() { return *ptr_; }
-		T* operator->() { return ptr_; }
+		char& operator*() { return *ptr_; }
+		char* operator->() { return ptr_; }
 		bool operator==(const iterator& rhs) { return ptr_ == rhs.ptr_; }
 		bool operator!=(const iterator& rhs) { return ptr_ != rhs.ptr_; }
 
 
 	private:
-		T* ptr_;
+		char* ptr_;
 	};
 
 	class const_iterator
@@ -26,47 +26,47 @@ public:
 	public:
 
 
-		const_iterator(T* ptr) : ptr_(ptr) { }
+		const_iterator(char* ptr) : ptr_(ptr) { }
 		const_iterator operator++() { const_iterator i = *this; ptr_++; return i; }
 		const_iterator operator++(int junk) { ptr_++; return *this; }
-		const T& operator*() { return *ptr_; }
-		const T* operator->() { return ptr_; }
+		const char& operator*() { return *ptr_; }
+		const char* operator->() { return ptr_; }
 		bool operator==(const const_iterator& rhs) { return ptr_ == rhs.ptr_; }
 		bool operator!=(const const_iterator& rhs) { return ptr_ != rhs.ptr_; }
 
 
 	private:
-		T* ptr_;
+		char* ptr_;
 	};
 
 	class reverse_iterator
 	{
 	public:
-		reverse_iterator(T* ptr) : ptr_(ptr) { }
+		reverse_iterator(char* ptr) : ptr_(ptr) { }
 		reverse_iterator operator++() { reverse_iterator i = *this; ptr_--; return i; }
 		reverse_iterator operator++(int junk) { ptr_--; return *this; }
-		T& operator*() { return *ptr_; }
-		T* operator->() { return ptr_; }
+		char& operator*() { return *ptr_; }
+		char* operator->() { return ptr_; }
 		bool operator==(const reverse_iterator& rhs) { return ptr_ == rhs.ptr_; }
 		bool operator!=(const reverse_iterator& rhs) { return ptr_ != rhs.ptr_; }
 
 	private:
-		T* ptr_;
+		char* ptr_;
 	};
 
 	class const_reverse_iterator
 	{
 	public:
-		const_reverse_iterator(T* ptr) : ptr_(ptr) { }
+		const_reverse_iterator(char* ptr) : ptr_(ptr) { }
 		const_reverse_iterator operator++() { const_reverse_iterator i = *this; ptr_--; return i; }
 		const_reverse_iterator operator++(int junk) { ptr_--; return *this; }
-		const T& operator*() { return *ptr_; }
-		const T* operator->() { return ptr_; }
+		const char& operator*() { return *ptr_; }
+		const char* operator->() { return ptr_; }
 		bool operator==(const const_reverse_iterator& rhs) { return ptr_ == rhs.ptr_; }
 		bool operator!=(const const_reverse_iterator& rhs) { return ptr_ != rhs.ptr_; }
 
 	private:
-		T* ptr_;
+		char* ptr_;
 	};
 
 	String()
